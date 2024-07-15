025cfc6c39bbd8239f09c764e6b7219fff65db60
Amanda Wegrzynkiewicz <amaenta@gmail.com>
2024-07-15 13:02:57 +0200

    Adding homepage controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Controller used to manage blog contents in the public part of the site.
+ */
+#[Route('/', name: 'homepage')]
+final class HomeController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+}
