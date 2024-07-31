7575b25e2031cffadd3f0cf18d234e1ebe6ef5d0
Amanda Wegrzynkiewicz <amaenta@gmail.com>
2024-07-31 12:48:35 +0200

    Adding setterfor plainPassword from register form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -107,6 +107,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this->plainPassword;
     }
 
+    public function setPlainPassword(string $plainPassword): static
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+
     /**
      * @see UserInterface
      */
