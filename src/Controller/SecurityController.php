c4dfe819e254268de1f8159a4d857515ce1d9eea
Amanda Wegrzynkiewicz <amaenta@gmail.com>
2024-08-01 21:26:29 +0200

    TokenGenerator as service, adding email verification at registry process

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use Doctrine\ORM\EntityManager;
+use App\Service\TokenGenerator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,6 +12,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
 class SecurityController extends AbstractController
 {
     /**
@@ -40,7 +43,7 @@ class SecurityController extends AbstractController
     /**
      * @Route("/register", name="app_register")
      */
-    public function registerUser(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    public function registerUser(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager, MailerInterface $mailer, TokenGenerator $tokenGenerator): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -51,8 +54,22 @@ class SecurityController extends AbstractController
             $user->setPassword($hashedPassword);
             $user->eraseCredentials();
 
+            $token = $tokenGenerator->generateToken();
+            $user->setVerificationToken($token);
+
             $entityManager->persist($user);
             $entityManager->flush();
+
+            $email = (new Email())
+                ->from('noreply@example.com')
+                ->to($user->getEmail())
+                ->subject('Please Verify Your Email')
+                ->html($this->renderView('emails/verification.html.twig', [
+                    'user' => $user,
+                    'token' => $token
+                ]));
+
+            $mailer->send($email);
 
             return $this->redirectToRoute('app_login');
         }
@@ -60,5 +77,11 @@ class SecurityController extends AbstractController
         return $this->render('security/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/verify/notice', name: 'app_verify_notice')]
+    public function verifyNotice(): Response
+    {
+        return new Response('Please check your email to verify your account.');
     }
 }
